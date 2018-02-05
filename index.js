c633dd4d233396e705d0c6e69573f080c77b843d
Tif Flowers <tif@gentlebull.com>
2018-02-05 21:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(days,holiday) {
+function holidayCountdown(Number,holiday) {
   return `It's ${days} days until ${holiday}!`;
 }
