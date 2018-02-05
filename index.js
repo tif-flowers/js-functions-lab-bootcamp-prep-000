351efa6f3168e37b498e5c78d602d796cf8d1916
Tif Flowers <tif@gentlebull.com>
2018-02-05 21:05:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays (string) {
   return "Happy holidays!"
 }
 
-function sayhappyHolidaysTo(string) {
+function happyHolidaysTo(string) {
   return (`Happy holidays, ${name}!`)
 }
